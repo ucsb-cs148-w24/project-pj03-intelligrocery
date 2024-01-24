7a337f053f824121ccfa213907bc0b867b2f8469
apzhou0 <apzhou@ucsb.edu>
2024-01-24 14:49:22 -0800

    Added template pages for Pantry, Recipes, and GroceryList

--- Intelligrocery/App.js
+++ Intelligrocery/App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+function GroceryListScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Hello CS148! (and the world too)</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Your Grocery List!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function PantryScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Your Pantry!</Text>
+    </View>
+  );
+}
+
+function RecipeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Recipes!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="GroceryList" component={GroceryListScreen} />
+        <Tab.Screen name="Pantry" component={PantryScreen} />
+        <Tab.Screen name="Recipes" component={RecipeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
