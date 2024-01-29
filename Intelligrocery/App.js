9a870b1deac7342fe65021687a23f7577fc49369
apzhou0 <apzhou@ucsb.edu>
2024-01-29 14:28:36 -0800

    Moves GroceryListScreen, PantryScreen, and RecipeScreen to a components folder

--- Intelligrocery/App.js
+++ Intelligrocery/App.js
@@ -1,31 +1,9 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-function GroceryListScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Your Grocery List!</Text>
-    </View>
-  );
-}
-
-function PantryScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Your Pantry!</Text>
-    </View>
-  );
-}
-
-function RecipeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Recipes!</Text>
-    </View>
-  );
-}
+import PantryScreen from "./components/PantryScreen";
+import GroceryListScreen from "./components/GroceryListScreen";
+import RecipeScreen from "./components/RecipeScreen";
 
 const Tab = createBottomTabNavigator();
 
---
+++ Intelligrocery/components/GroceryListScreen.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+function GroceryListScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Your Grocery List!</Text>
+    </View>
+  );
+}
+
+export default GroceryListScreen;
