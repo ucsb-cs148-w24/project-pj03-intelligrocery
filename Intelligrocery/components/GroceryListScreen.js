4b4675eeb5236e70147c50942b09f05eb3cb9608
Anderson Lee <andersonlee@ucsb.edu>
2024-01-31 17:57:41 -0800

    added search bar on recipe page

--- Intelligrocery/components/GroceryListScreen.js
+++ Intelligrocery/components/GroceryListScreen.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, Button, ScrollView, StyleSheet } from 'react-native';
 
-function GroceryListScreen() {
+function GroceryListScreen () {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Your Grocery List!</Text>
     </View>
   );
-}
+};
 
 export default GroceryListScreen;
--- Intelligrocery/components/RecipeScreen.js
+++ Intelligrocery/components/RecipeScreen.js
@@ -1,12 +1,67 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, Button, ScrollView, StyleSheet } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 
-function RecipeScreen() {
+export default class RecipeScreen extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
+
+  handleEnter = () => {
+    console.log('Searching...', this.state.search);
+  }
+
+  render() {
+    const { search } = this.state;
+    return (
+      <View>
+        <SearchBar
+        placeholder="Type Here..."
+        onChangeText={this.updateSearch}
+        value={search}
+        />
+        <Button
+        title ='Enter'
+        onPress={this.handleEnter}
+        ></Button>
+        
+        </View>
+    );
+  }
+}
+  /*const [recipe, setRecipe] = useState("");
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Recipes!</Text>
+    <View style = {styles.container}>
+      <View style = {styles.inputContainer}>
+        <TextInput style = {styles.userInput}
+        placeholder='Search a recipe...'
+        value = {recipe}
+        onChangeText={(text) => setRecipe(text)}></TextInput>
+      </View>
     </View>
   );
-}
+};
 
-export default RecipeScreen;
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    padding: 20
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  userInput: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: '#777',
+    borderRadius: 10,
+    fontSize: 36
+  }
+})*/
