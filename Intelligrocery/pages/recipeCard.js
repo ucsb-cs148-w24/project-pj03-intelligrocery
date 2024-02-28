e65651ad1ec9590544f900c63bf1df9eca5fe965
nadavavital <nadavavital@ucsb.edu>
2024-02-28 14:36:14 -0800

    removed large comment

--- Intelligrocery/pages/recipeCard.js
+++ Intelligrocery/pages/recipeCard.js
@@ -1,30 +1,3 @@
-// import React from 'react';
-// import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
-// import styles from '../styles/styles';
-// import { Spacer } from 'react-native-flex-layout';
-
-// const RecipeCard = ({ imageSource, title, numIngredients, recipeLink }) => {
-//     const handlePress = () => {
-//         Linking.openURL(recipeLink);
-//     };
-
-//     return (
-//         <TouchableOpacity onPress={handlePress}>
-//             <View style = {styles.recipeCard}>
-//                 <Image style = {styles.recipeCardImage} source={{uri: imageSource}} />
-//                 <View style = {styles.recipeDetails}>
-//                     <Text style = {styles.recipeCardTitle}>{title}</Text>
-//                     <Text style = {styles.recipeCardNumIngredients}>{numIngredients} ingredients</Text>
-//                 </View>
-//                 <Spacer />
-//             </View>
-//         </TouchableOpacity>
-//     );
-// };
-
-// export default RecipeCard;
-
-
 import React from 'react';
 import { View, Text, Image, TouchableOpacity, Linking } from 'react-native';
 import styles from '../styles/styles';
